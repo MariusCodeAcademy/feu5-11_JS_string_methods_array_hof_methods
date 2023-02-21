e71a2f65bdd7a3767edd8b60fb603a5c6da2e136
Marius CA <marius@example.com>
2023-02-21 10:26:37 +0200

    slice

---
+++ js/stringMethods.js
@@ -0,0 +1,14 @@
+'use strict';
+console.log('stringMethods.js file was loaded');
+
+//                 0123456789
+const pvzString = 'I am Mike from Uk';
+
+console.log('pvzString ===', pvzString);
+
+// String.slice(start, end)
+// gauti 'I am'
+const iAm = pvzString.slice(0, 4);
+console.log('iAm ===', iAm);
+
+// gauti 'from' zodeli is stringo su slice neigiamais argumentais
