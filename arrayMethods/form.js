19ac03a47be0ddb320e72bfb20fc434db5e47c4a
Marius CA <marius@example.com>
2023-02-21 17:00:32 +0200

    form

---
+++ arrayMethods/form.js
@@ -0,0 +1,12 @@
+'use strict';
+console.log('form.js file was loaded');
+
+// taikomes
+const formEl = document.getElementById('kartokForm');
+
+formEl.addEventListener('submit', (event) => {
+  // sustabdyt perkrovima
+  event.preventDefault();
+  console.log('js control the form');
+  //tolimesne logika
+});
