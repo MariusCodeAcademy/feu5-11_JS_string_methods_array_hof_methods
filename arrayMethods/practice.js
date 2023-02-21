11cde384ec9468c1062c41bac4e7e6b94efc184b
Marius CA <marius@example.com>
2023-02-21 15:39:48 +0200

    addH3ToHtml

--- arrayMethods/practice.js
+++ arrayMethods/practice.js
@@ -25,7 +25,51 @@ numArray.forEach(function (sk) {
   //   allSuperPositiveArr.push(sk);
   // }
 
+  // ternary version
+  allSuperPositiveArr.push(sk < 0 ? sk * -1 : sk);
+
   // kitas budas
-  allSuperPositiveArr.push(Math.abs(sk));
+  // allSuperPositiveArr.push(Math.abs(sk));
 });
 console.log('allSuperPositiveArr ===', allSuperPositiveArr);
+
+// 3. parasyti funkcija kuri grazina vidurki numArray
+
+function getAvg(arr) {
+  let total = 0;
+  arr.forEach((sk) => {
+    total += sk;
+  });
+  console.log('total ===', total);
+  const avg = total / arr.length;
+  console.log('avg ===', avg.toFixed(2));
+}
+
+const skArr = [1, 2, 3, 4, 5, 6];
+
+getAvg(numArray);
+getAvg(skArr);
+
+// 3. sukurti funkcija kuri argumentu gauna stringa pvz 'lape' ir sukuria html h3 elementa su viduje esanciu tekstu kurio pirmoji raide paversta i didziaja ir pataplina elementa i dokumento body apacia pvz `<h3>Lape</h3>`
+
+const makeFirstCappital = (strToBeTransformed) => {
+  return (
+    strToBeTransformed.charAt(0).toUpperCase() + strToBeTransformed.slice(1)
+  );
+};
+const didSer = makeFirstCappital('serbentautas');
+console.log('didSer ===', didSer);
+
+function addH3ToHtml(str) {
+  // sukurti h3 elementa
+  const h3El = document.createElement('h3');
+  // prideti jam texta padidinta raide
+  h3El.textContent = makeFirstCappital(str);
+  console.log('h3El ===', h3El);
+  console.dir(h3El);
+  h3El.style.padding = '16px';
+  // patalpinti body pabaiga
+  document.body.appendChild(h3El);
+}
+addH3ToHtml('lape');
+addH3ToHtml('serbentautas');
